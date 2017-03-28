7b8a6fb6a357d3dc43f6e209bed43cf7e03a1ab9
Louis Romero <lpromero@google.com>
2017-03-28 12:56:36 +0200

    Silence a performSelector-leaks warning.
    This closes https://github.com/google/gtm-session-fetcher/issues/76.

--- Source/GTMReadMonitorInputStream.m
+++ Source/GTMReadMonitorInputStream.m
@@ -94,6 +94,8 @@
         // and invoke on the proper thread.
         SEL sel = @selector(invokeReadSelectorWithBuffer:);
         NSData *data = [NSData dataWithBytes:buffer length:(NSUInteger)numRead];
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
         if (_runLoopModes) {
           [self performSelector:sel
                        onThread:_thread
@@ -106,6 +108,7 @@
                      withObject:data
                   waitUntilDone:NO];
         }
+#pragma clang diagnostic pop
       }
     }
   }
