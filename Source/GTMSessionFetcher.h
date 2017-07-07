2c7bcea421e126046c6b6fd23882c129132a2c35
Thomas Van Lenten <thomasvl@google.com>
2017-07-07 12:56:20 -0400

    Support building for watchOS, add framework for watchOS

--- Source/GTMSessionFetcher.h
+++ Source/GTMSessionFetcher.h
@@ -678,7 +678,7 @@ NSData * GTM_NULLABLE_TYPE GTMDataFromInputStream(NSInputStream *inputStream, NS
 @end
 #endif  // GTM_FETCHER_AUTHORIZATION_PROTOCOL
 
-#if TARGET_OS_IPHONE
+#if GTM_BACKGROUND_TASK_FETCHING
 // A protocol for an alternative target for messages from GTMSessionFetcher to UIApplication.
 // Set the target using +[GTMSessionFetcher setSubstituteUIApplication:]
 @protocol GTMUIApplicationProtocol <NSObject>
@@ -795,7 +795,7 @@ NSData * GTM_NULLABLE_TYPE GTMDataFromInputStream(NSInputStream *inputStream, NS
 // dictionary is stored as identifier metadata.
 - (GTM_NULLABLE GTM_NSDictionaryOf(NSString *, NSString *) *)sessionIdentifierMetadata;
 
-#if TARGET_OS_IPHONE
+#if TARGET_OS_IPHONE && !TARGET_OS_WATCH
 // The app should pass to this method the completion handler passed in the app delegate method
 // application:handleEventsForBackgroundURLSession:completionHandler:
 + (void)application:(UIApplication *)application
@@ -1146,12 +1146,12 @@ NSData * GTM_NULLABLE_TYPE GTMDataFromInputStream(NSInputStream *inputStream, NS
 // size of the response data (e.g. a 1-byte response can only be divided into one chunk).
 @property(atomic, readwrite) NSUInteger testBlockAccumulateDataChunkCount;
 
-#if TARGET_OS_IPHONE
+#if GTM_BACKGROUND_TASK_FETCHING
 // For testing or to override UIApplication invocations, apps may specify an alternative
 // target for messages to UIApplication.
 + (void)setSubstituteUIApplication:(nullable id<GTMUIApplicationProtocol>)substituteUIApplication;
 + (nullable id<GTMUIApplicationProtocol>)substituteUIApplication;
-#endif  // TARGET_OS_IPHONE
+#endif  // GTM_BACKGROUND_TASK_FETCHING
 
 // Exposed for testing.
 + (GTMSessionCookieStorage *)staticCookieStorage;
---
+++ Source/GTMSessionFetcherwatchOS/GTMSessionFetcherwatchOS.h
@@ -0,0 +1,12 @@
+//
+//  GTMSessionFetcherwatchOS.h
+//
+
+#import "GTMSessionFetcher.h"
+#import "GTMSessionFetcherService.h"
+#import "GTMSessionUploadFetcher.h"
+#import "GTMSessionFetcherService.h"
+#import "GTMSessionFetcherLogging.h"
+#import "GTMGatherInputStream.h"
+#import "GTMMIMEDocument.h"
+#import "GTMReadMonitorInputStream.h"
