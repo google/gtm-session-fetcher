google/gtm-session-fetcher

commit b5e3d4115386e8628e88383fe21973cf7e00c6d4

GTMSessionUploadFetcher - Handle empty and non-existing files to upload (issue #140) (#141)
Handle empty files to upload; fail with an error on non-existing file upload

Source/GTMSessionUploadFetcher.m
@@ -1455,11 +1455,13 @@ NSString *const kGTMSessionFetcherUploadLocationObtainedNotification =
 
   // TODO
   // Maybe here we can check to see if the request had x goog content length set. (the file length one).
-  int64_t previousContentLength =
-      [[chunkFetcher.request valueForHTTPHeaderField:@"Content-Length"] longLongValue];
+  NSString *previousContentLengthValue =
+      [chunkFetcher.request valueForHTTPHeaderField:@"Content-Length"];
   // The Content-Length header may not be present if the chunk fetcher was recreated from
   // a background session.
-  BOOL hasKnownChunkSize = (previousContentLength > 0);
+  BOOL hasKnownChunkSize = (previousContentLengthValue != nil);
+  int64_t previousContentLength = [previousContentLengthValue longLongValue];
+
   BOOL needsQuery = (!hasKnownChunkSize && !isUploadStatusStopped);
 
   if (error || (needsQuery && !isQueryFetch)) {
@@ -1504,7 +1506,9 @@ NSString *const kGTMSessionFetcherUploadLocationObtainedNotification =
                             chunkFetcher, chunkFetcher.request.allHTTPHeaderFields,
                             responseHeaders);
 #endif
-    if (isUploadStatusStopped || (_currentOffset > _uploadFileLength && _uploadFileLength > 0)) {
+    if (isUploadStatusStopped ||
+        _uploadFileLength == 0 ||
+        (_currentOffset > _uploadFileLength && _uploadFileLength > 0)) {
       // This was the last chunk.
       if (error == nil && uploadStatus == kStatusCancelled) {
         // Report cancelled status as an error.

Source/UnitTests/GTMSessionFetcherChunkedUploadTest.m
@@ -963,6 +963,103 @@ static void TestProgressBlock(GTMSessionUploadFetcher *fetcher,
   [self removeTemporaryFileURL:bigFileURL];
 }
 
+- (void)testEmptyFileURLUploadFetch {
+  // Like the previous, but we upload in a single chunk, needed for an out-of-process upload.
+  FetcherNotificationsCounter *fnctr = [[FetcherNotificationsCounter alloc] init];
+
+  NSURL *emptyFileURL =
+      [self fileToUploadURLWithData:[NSData data] baseName:NSStringFromSelector(_cmd)];
+
+  NSURLRequest *request = [self validUploadFileRequest];
+  GTMSessionUploadFetcher *fetcher =
+  [GTMSessionUploadFetcher uploadFetcherWithRequest:request
+                                     uploadMIMEType:@"text/plain"
+                                          chunkSize:kGTMSessionUploadFetcherStandardChunkSize
+                                     fetcherService:_service];
+  fetcher.uploadFileURL = emptyFileURL;
+  fetcher.useBackgroundSession = NO;
+  fetcher.allowLocalhostRequest = YES;
+
+  XCTestExpectation *expectation = [self expectationWithDescription:@"fetched"];
+  [fetcher beginFetchWithCompletionHandler:^(NSData *data, NSError *error) {
+    // Test that server result is returned (success or failure).
+    // The current test server don't accept POST requests with empty body.
+    XCTAssertNil(data);
+    XCTAssertEqual(error.code, 503);
+    [expectation fulfill];
+  }];
+
+  [self waitForExpectationsWithTimeout:_timeoutInterval handler:nil];
+  [self assertCallbacksReleasedForFetcher:fetcher];
+
+  // Check that we uploaded the expected chunks.
+  NSArray *expectedURLStrings = @[ @"/gettysburgaddress.txt.upload" ];
+  NSArray *expectedCommands = @[ @"finalize" ];
+  NSArray *expectedOffsets = @[ @0 ];
+  NSArray *expectedLengths = @[ @0 ];
+  XCTAssertEqualObjects(fnctr.uploadChunkRequestPaths, expectedURLStrings);
+  XCTAssertEqualObjects(fnctr.uploadChunkCommands, expectedCommands);
+  XCTAssertEqualObjects(fnctr.uploadChunkOffsets, expectedOffsets);
+  XCTAssertEqualObjects(fnctr.uploadChunkLengths, expectedLengths);
+
+  XCTAssertEqual(fnctr.fetchStarted, 2);
+  XCTAssertEqual(fnctr.fetchStopped, 2);
+  XCTAssertEqual(fnctr.uploadChunkFetchStarted, 1);
+  XCTAssertEqual(fnctr.uploadChunkFetchStopped, 1);
+  XCTAssertEqual(fnctr.retryDelayStarted, 0);
+  XCTAssertEqual(fnctr.retryDelayStopped, 0);
+  XCTAssertEqual(fnctr.uploadLocationObtained, 1);
+
+  [self removeTemporaryFileURL:emptyFileURL];
+}
+
+- (void)testNonExistingFileURLUploadFetch {
+  // Like the previous, but we upload in a single chunk, needed for an out-of-process upload.
+  FetcherNotificationsCounter *fnctr = [[FetcherNotificationsCounter alloc] init];
+
+  NSURL *nonExistingFileURL = [NSURL fileURLWithPath:@"some/file"];
+
+  NSURLRequest *request = [self validUploadFileRequest];
+  GTMSessionUploadFetcher *fetcher =
+  [GTMSessionUploadFetcher uploadFetcherWithRequest:request
+                                     uploadMIMEType:@"text/plain"
+                                          chunkSize:kGTMSessionUploadFetcherStandardChunkSize
+                                     fetcherService:_service];
+  fetcher.uploadFileURL = nonExistingFileURL;
+  fetcher.useBackgroundSession = NO;
+  fetcher.allowLocalhostRequest = YES;
+
+  XCTestExpectation *expectation = [self expectationWithDescription:@"fetched"];
+  [fetcher beginFetchWithCompletionHandler:^(NSData *data, NSError *error) {
+    // The current test server don't accept POST requests with empty body.
+    XCTAssertNil(data);
+    XCTAssertEqual(error.code, 260);
+    [expectation fulfill];
+  }];
+
+  [self waitForExpectationsWithTimeout:_timeoutInterval handler:nil];
+
+  [self assertCallbacksReleasedForFetcher:fetcher];
+
+  // Check that we uploaded the expected chunks.
+  NSArray *expectedURLStrings = @[];
+  NSArray *expectedCommands = @[];
+  NSArray *expectedOffsets = @[];
+  NSArray *expectedLengths = @[];
+  XCTAssertEqualObjects(fnctr.uploadChunkRequestPaths, expectedURLStrings);
+  XCTAssertEqualObjects(fnctr.uploadChunkCommands, expectedCommands);
+  XCTAssertEqualObjects(fnctr.uploadChunkOffsets, expectedOffsets);
+  XCTAssertEqualObjects(fnctr.uploadChunkLengths, expectedLengths);
+
+  XCTAssertEqual(fnctr.fetchStarted, 1);
+  XCTAssertEqual(fnctr.fetchStopped, 1);
+  XCTAssertEqual(fnctr.uploadChunkFetchStarted, 0);
+  XCTAssertEqual(fnctr.uploadChunkFetchStopped, 0);
+  XCTAssertEqual(fnctr.retryDelayStarted, 0);
+  XCTAssertEqual(fnctr.retryDelayStopped, 0);
+  XCTAssertEqual(fnctr.uploadLocationObtained, 1);
+}
+
 - (void)testBigDataChunkedUploadFetch {
   FetcherNotificationsCounter *fnctr = [[FetcherNotificationsCounter alloc] init];
 

commit bed35776b7058bd50a25a3e01ed8a2172d74efa7

Fix upload fetcher when using upload data. Broken by PR #141.

Source/GTMSessionUploadFetcher.m
@@ -1507,7 +1507,7 @@ NSString *const kGTMSessionFetcherUploadLocationObtainedNotification =
                             responseHeaders);
 #endif
     if (isUploadStatusStopped ||
-        _uploadFileLength == 0 ||
+        (!_uploadData && _uploadFileLength == 0) ||
         (_currentOffset > _uploadFileLength && _uploadFileLength > 0)) {
       // This was the last chunk.
       if (error == nil && uploadStatus == kStatusCancelled) {