43bc73f62df6e5d234cdbc5cc8d24e77d493afdc
Paul Beusterien <paulbeusterien@google.com>
2020-09-13 11:20:06 -0700

    Fix platform build warning

--- Package.swift
+++ Package.swift
@@ -4,12 +4,6 @@ import PackageDescription
 
 let package = Package(
     name: "GTMSessionFetcher",
-    platforms: [
-        .macOS(.v10_10),
-        .iOS(.v8),
-        .tvOS(.v9),
-        .watchOS(.v2)
-    ],
     products: [
         .library(
             name: "GTMSessionFetcher",
