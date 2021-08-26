b413b1e4b5060f630c20a600c1a4d693ea61fc10
Michael Wyman <michael@mwyman.com>
2021-08-26 11:54:08 -0500

    Add comment warning that the `callbackQueue` on `GTMSessionFetcher` must be serial.

--- Source/GTMSessionFetcher.h
+++ Source/GTMSessionFetcher.h
@@ -1102,7 +1102,10 @@ NSData *_Nullable GTMDataFromInputStream(NSInputStream *inputStream, NSError **o
 // Log of request and response, if logging is enabled
 @property(atomic, copy, nullable) NSString *log;
 
-// Callbacks are run on this queue.  If none is supplied, the main queue is used.
+// Callbacks are run on this queue. If none is supplied, the main queue is used.
+//
+// CAUTION: This block MUST be a serial queue. Setting a concurrent queue may result in callbacks
+// arriving out-of-order.
 @property(atomic, strong, null_resettable) dispatch_queue_t callbackQueue;
 
 // The queue used internally by the session to invoke its delegate methods in the fetcher.
