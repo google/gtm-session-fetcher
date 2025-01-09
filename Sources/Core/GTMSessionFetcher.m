b522ee3f044211d6a191dc278dcf249f371936c9
Thomas Van Lenten <thomasvl@google.com>
2025-01-09 13:33:46 -0500

    Correct the type on sessionIdentifierMetadata.
    This has never been limited to `NSString` values, and if it was called from
    Swift it likely would cause problems. Luckily, it is really meant to be an
    internal api so clients shouldn't be calling it. But since it is public, it is a
    breaking change to Swift.
    
    Part of #398

--- Sources/Core/GTMSessionFetcher.m
+++ Sources/Core/GTMSessionFetcher.m
@@ -1744,7 +1744,7 @@ NSData *_Nullable GTMDataFromInputStream(NSInputStream *inputStream, NSError **o
   }
 }
 
-- (nullable NSDictionary *)sessionIdentifierMetadata {
+- (nullable NSDictionary<NSString *, id> *)sessionIdentifierMetadata {
   @synchronized(self) {
     GTMSessionMonitorSynchronized(self);
 
@@ -1752,7 +1752,7 @@ NSData *_Nullable GTMDataFromInputStream(NSInputStream *inputStream, NSError **o
   }
 }
 
-- (nullable NSDictionary *)sessionIdentifierMetadataUnsynchronized {
+- (nullable NSDictionary<NSString *, id> *)sessionIdentifierMetadataUnsynchronized {
   GTMSessionCheckSynchronized(self);
 
   // Session Identifier format: "com.google.<ClassName>_<UUID>_<Metadata in JSON format>
