cf4272caeb392b4dffae3ed263363a5326340022
Thomas Van Lenten <thomasvl@google.com>
2025-07-07 10:51:00 -0400

    Revert "Support iOS 12 again."
    This reverts commit a9f292ecc240ca8760b3654ef30469f11a864672.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "GTMSessionFetcher",
     platforms: [
-        .iOS(.v12),
+        .iOS(.v13),
         .macOS(.v10_15),
         .tvOS(.v13),
         .watchOS(.v7)
